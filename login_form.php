79f314b9d3bcc0e99c0eba1a20cbb2fdfa1ebfe9
kavindu yasintha silva <skysilva@students.nsbm.lk>
2020-01-13 12:48:58 +0530

    Add files via upload

---
+++ login_form.php
@@ -0,0 +1,64 @@
+<?php
+
+if (isset($_POST["login_user_with_product"])) {
+
+	$product_list = $_POST["product_id"];
+	
+	$json_e = json_encode($product_list);
+	
+	setcookie("product_list",$json_e,strtotime("+1 day"),"/","","",TRUE);
+
+}
+?>
+
+	<div class="wait overlay">
+		<div class="loader"></div>
+	</div>
+	<div class="container-fluid">
+				<!-- row -->
+				
+
+					<div class="login-marg">
+						
+						
+						
+								<form onsubmit="return false" id="login" class="login100-form ">
+									<div class="billing-details jumbotron">
+                                    <div class="section-title">
+                                        <h2 class="login100-form-title " >Login Here</h2>
+                                    </div>
+                                   
+                                    
+                                    <div class="form-group">
+                                       <label for="email">Email</label>
+                                        <input class="input input-borders" type="email" name="email" placeholder="Email" id="password" required>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                       <label for="email">Password</label>
+                                        <input class="input input-borders" type="password" name="password" placeholder="password" id="password" required>
+                                    </div>
+                                    
+                                    <div class="text-pad" >
+                                       <a href="store.php">
+                                           forget password ?
+                                       </a>
+                                        
+                                    </div>
+                                    
+                                        <input class="primary-btn btn-block"   type="submit"  Value="Login">
+                                        
+                                        <div class="panel-footer"><div class="alert alert-danger"><h4 id="e_msg"></h4></div></div>
+                                    
+                                    	
+                                        
+                                    
+
+                                </div>
+                                
+								</form>
+                       
+					</div>
+
+					
+			</div>
---
+++ store.php
@@ -0,0 +1,148 @@
+<?php
+include 'header.php';
+include 'db.php'
+?>
+     <script id="jsbin-javascript">
+(function (global) {
+	if(typeof (global) === "undefined")
+	{
+		throw new Error("window is undefined");
+	}
+    var _hash = "!";
+    var noBackPlease = function () {
+        global.location.href += "#";
+	
+        global.setTimeout(function () {
+            global.location.href += "!";
+        }, 50);
+    };	
+	// Earlier we had setInerval here....
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+    global.onload = function () {        
+		noBackPlease();
+		// disables backspace on page except on input fields and textarea..
+		document.body.onkeydown = function (e) {
+            var elm = e.target.nodeName.toLowerCase();
+            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                e.preventDefault();
+            }
+            // stopping event bubbling up the DOM tree..
+            e.stopPropagation();
+        };		
+    };
+})(window);
+</script>
+      <div class="main main-raised"> 
+        
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<!-- ASIDE -->
+					<div id="aside" class="col-md-3">
+						
+						<div id="get_category">
+				        </div>
+				
+
+						<!-- aside Widget -->
+						<div class="aside">
+							
+							<div class="price-filter">
+								<div id="" class=""><div class=""><div class="" style=""><div></div></div></div></div>
+								<div class="input-number price-min">
+								
+									
+								</div>
+								<span>-</span>
+								
+							</div>
+						</div>
+						<!-- /aside Widget -->
+
+						<!-- aside Widget -->
+						<div id="get_brand">
+				        </div>
+						<!-- /aside Widget -->
+
+						<!-- aside Widget -->
+						<div class="aside">
+							<h3 class="aside-title">Top selling</h3>
+							<div id="get_product_home">
+						
+								
+							
+							</div>
+						</div>
+					
+					</div>
+				
+
+			
+					<div id="store" class="col-md-9">
+					
+						<div class="store-filter clearfix">
+							<div class="store-sort">
+								<label>
+									Sort By:
+									<select class="input-select">
+										<option value="0">Popular</option>
+										<option value="1">Position</option>
+									</select>
+								</label>
+
+								<label>
+									Show:
+									<select class="input-select">
+										<option value="84">20</option>
+										<option value="100">50</option>
+									</select>
+								</label>
+							</div>
+							<ul class="store-grid">
+								<li class="active"><i class="fa fa-th"></i></li>
+								<li><a href="#"><i class="fa fa-th-list"></i></a></li>
+							</ul>
+						</div>
+						<!-- /store top filter -->
+
+						<!-- store products -->
+						<div class="row" id="product-row">
+						<div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
+							<!-- product -->
+							<div id="get_product">
+							<!--Here we get product jquery Ajax Request-->
+						</div>
+							
+							<!-- /product -->
+						</div>
+						<!-- /store products -->
+
+						<!-- store bottom filter -->
+						<div class="store-filter clearfix">
+							<span class="store-qty">Showing 20-100 products</span>
+							<ul class="store-pagination" id="pageno">
+								<li ><a class="active" href="#aside">1</a></li>
+								
+								<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+							</ul>
+						</div>
+						<!-- /store bottom filter -->
+					</div>
+					<!-- /STORE -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+</div>
+<?php
+include "newslettter.php";
+include "footer.php";
+?>
