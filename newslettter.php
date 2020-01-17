65e31e89059f55f98ed87d8cc320a72c8ad3a202
Naduntha Thathsara Wedage <59860881+naduntha99@users.noreply.github.com>
2020-01-17 10:39:58 +0530

    Update newslettter.php

--- newslettter.php
+++ newslettter.php
@@ -2,7 +2,7 @@
 
         
 
-        <div id="newsletter" class="section">
+        <div id="newspeper" class="section">
 			<!-- container -->
 			<div class="container">
 				<!-- row -->
@@ -13,20 +13,20 @@
 							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
 							<form id="offer_form" onsubmit="return false">
 								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
-								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+								<button class="newspeper-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
 							</form>
 							<div class="" id="offer_msg">
                                 <!--Alert from signup form-->
                             </div>
-							<ul class="newsletter-follow">
+							<ul class="newspeper-follow">
 								<li>
-									<a href=""><i class="fa fa-facebook"></i></a>
+									<a href=""><i class="fa fa-fb"></i></a>
 								</li>
 								<li>
 									<a href=""><i class="fa fa-twitter"></i></a>
 								</li>
 								<li>
-									<a href=""><i class="fa fa-instagram"></i></a>
+									<a href=""><i class="fa fa-inster"></i></a>
 								</li>
 								<li>
 									<a href=""><i class="fa fa-github"></i></a>
