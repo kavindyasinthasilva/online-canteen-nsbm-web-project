0d9401c9715ba5ec1600d7b7835ce71fefd49871
Naduntha Thathsara Wedage <59860881+naduntha99@users.noreply.github.com>
2020-01-17 11:19:03 +0530

    Update error.php

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
   <div class="error">
   	<?php foreach ($errors as $error) : ?>
-  	  <p><?php echo $error ?></p>
+    <p><?php echo $error ?></p>
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
